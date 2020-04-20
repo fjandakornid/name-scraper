ef0dcc709254fe45ccbb9803f9e2ce9a0fcb749f
gudjonm <gudjonm@advania.is>
2020-04-20 22:44:18 +0000

    minor refactoring. removed unused types package. added lint config

--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@ import cheerio from 'cheerio'
 const urlGirls = 'https://vefur.island.is/mannanofn/leit-ad-nafni/?Stafrof=&Nafn=&Stulkur=on&Samthykkt=yes'
 const urlBoys = 'https://vefur.island.is/mannanofn/leit-ad-nafni/?Stafrof=&Nafn=&Drengir=on&Samthykkt=yes'
 
-interface nameItem {
+interface NameItem {
   name: string,
   gender: number,
   approved: string
 }
 
 async function main() {
-  let [boys, girls] = await Promise.all([
+  const [boys, girls] = await Promise.all([
     getNames(urlBoys, 0),
     getNames(urlGirls, 1)
   ])
@@ -19,18 +19,17 @@ async function main() {
   console.log(names)
 }
 
-async function getNames(url: string, gender: number): Promise<nameItem[]> {
-  console.log('begin ' + gender)
+async function getNames(url: string, gender: number): Promise<NameItem[]> {
   const request = bent('string')
   const content = await request(url)
   const $ = cheerio.load(content)
-  
-  return $('.nametype > ul > li').toArray().map((li) => {
+
+  return $('.nametype > ul > li').toArray().map(li => {
     const line = $(li).text().trim()
     const items = line.split(' ')
     const name = items[0]
     const approved = items.length > 1 ? items[items.length - 1] : null
-    return { name, gender, approved } as nameItem
+    return { name, gender, approved } as NameItem
   })
 }
 
